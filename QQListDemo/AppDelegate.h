bc4663cebe9a5167ddaa886e8ba54e895433a4eb
jamalping <lixiaoping@iMac.local>
2015-05-14 17:34:28 +0800

    Initial Commit

---
+++ QQListDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQListDemo
+//
+//  Created by jamalping on 15/5/14.
+//  Copyright (c) 2015年 jamal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQListDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQListDemo
+//
+//  Created by jamalping on 15/5/14.
+//  Copyright (c) 2015年 jamal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
